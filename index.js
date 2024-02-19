9ec40eae74e734f407904168dcddbbf2582ca4c5
akasharyan58 <akasharyan58@gmail.com>
2024-02-19 20:52:14 +0530

    images upgradation

--- index.js
+++ index.js
@@ -9,13 +9,38 @@ const weatherapp= async(city)=>{
     const data= await fetch(url);
     const res= await data.json();
     console.log(res);
+    //console.log("hi");
     document.querySelector(".city-name").innerHTML=res.name;
     document.querySelector(".temp").innerHTML=Math.round(res.main.temp-273)+"°C";
     document.querySelector(".humidity").innerHTML=res.main.humidity+ "%";
     document.querySelector(".wind").innerHTML=res.wind.speed+ "km/h";
+    const weather=res.weather[0].main;
+    const img_src=document.querySelector(".weather_icon");
+  //  console.log(weather);
+    if(weather=='Clouds')
+    {
+        img_src.src="images/clouds.png";
+    }
+    else if(weather=="Clear")
+    {
+        img_src.src="images/clear.png";
+    }
+    else if(weather=="Mist")
+    {
+        img_src.src="images/mist.png";
+    }
+    else if(weather=="Drizzle")
+    {
+        img_src.src="images/drizzle.png";
+    }
+     else if(weather=="Rain")
+    {
+        img_src.src="images/rain.png";
+    }
+    console.log(img_src.src +" kk");
 };
 
 searchbtn.addEventListener("click", ()=>{
-    console.log(searchbox.value);
+    //console.log(searchbox.value);
     weatherapp(searchbox.value);
 })
